d9a4b10a6e5146738636983f1a21e79c784d0096
jara <jara@jaraarch>
2020-07-26 19:03:31 -0600

    Parte logica del funcionamiento que permite comparar cadenas de ADN

--- Decodificador-ADN/src/Main.java
+++ Decodificador-ADN/src/Main.java
@@ -1,3 +1,6 @@
+
+import Backend.Logica;
+
 /**
  *
  * @author jara
@@ -8,7 +11,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Logica lc = new Logica();
+        lc.cadenas();
     }
     
 }
